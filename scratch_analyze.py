a5b16a4682a3e3fb9b3005d54c175c69ed2b7a50
HgCgYn <andy0625huang@gmail.com>
2026-06-14 20:43:07 +0800

    chore: definitively remove junk files from git tracking

--- scratch_analyze.py
+++
@@ -1,6 +0,0 @@
-import csv
-
-with open('experiments/results_yolo_gpt.csv', encoding='utf-8') as f:
-    for row in csv.DictReader(f):
-        if row['vlm_triggered'] == '1' and row['vlm_label_correct'] == '0':
-            print(f"{row['image_filename']:<35} | GT={row['ground_truth_label']:<8} | YOLO={row['yolo_label']:<15} | VLM={row['vlm_label']}")
